3a7c9346354122714f53b2311db2e1da9aca838e
Tomo-Aot <vespaanalis833@gmail.com>
2025-09-30 20:46:51 +0900

    Add files via upload

---
+++ image_date_display.R
@@ -0,0 +1,49 @@
+# 2025-09-30
+# loading packages
+library(exifr)
+library(tidyverse)
+
+path = "./data/image_sample.JPG"
+
+info = read_exif(path)
+
+info$DateTimeOriginal |> 
+  as_datetime()
+
+file.info(path)
+
+# 画像ファイルから撮影日の情報を取得して、
+# 元の画像の右上に表示して保存する関数を作成
+
+image_date_display = function(path) {
+  library(magick)
+  
+  # loading file information
+  info = file.info(path)
+  rec_date = info$mtime |> 
+    as.Date()
+  
+  # loading image file
+  img = image_read(path)
+  
+  img_annt = image_annotate(
+    img, 
+    text = rec_date,
+    size = 200,
+    color = "white",
+    font = "Noto Sans",
+    boxcolor = "darkgray",
+    gravity = "northeast"
+  )
+  
+  # SAVE IMAGE
+  savefile = str_replace(path, ".JPG", "test.JPG")
+  image_write(image = img_annt, path = savefile)
+}
+
+
+image_date_display(path = path)
+
+
+
+
